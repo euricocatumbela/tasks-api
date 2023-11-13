a20dde580aedce049100f3911ceac8158b762c4e
Eurico Catumbela <erickjeremmy@gmail.com>
2023-11-13 20:59:13 +0200

    Added user and task entity

---
+++ src/entity/Task.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
+
+@Entity()
+
+export class Task {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  completed: boolean;
+ 
+  @ManyToOne(() => User, user => user.tasks)
+  user: User;
+}
