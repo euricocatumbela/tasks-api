93d1a0a3e9fe076a2e096c4afb63749653c9324c
Eurico Catumbela <erickjeremmy@gmail.com>
2023-11-13 21:02:41 +0200

    Added index file to handle apollo server

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { ApolloServer } from 'apollo-server';
+import schema  from './graphql';
+const PORT = process.env.PORT || 4000;
+
+async function startServer() {
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+
+
+  const { url } = await server.listen(PORT);
+  console.log(`Server is running, GraphQL Playground available at ${url}`);
+}
+
+startServer();
